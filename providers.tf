980dfa15b9ac4e25fc0faf68b48cc1c3b6fe09e3
SaponenkoVS <saponenkovladislav2001@mail.ru>
2024-05-26 13:14:12 +0200

    init

--- providers.tf
+++ providers.tf
@@ -15,4 +15,9 @@ provider "aws" {
   endpoints {
     s3 = "http://localhost:4566"
   }
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
 }
