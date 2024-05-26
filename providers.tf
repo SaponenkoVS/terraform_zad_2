5bf47b4c29ddcf38455fe9ba42de36849b4b97a2
SaponenkoVS <saponenkovladislav2001@mail.ru>
2024-05-26 13:08:50 +0200

    init

--- providers.tf
+++ providers.tf
@@ -11,6 +11,8 @@ provider "aws" {
   access_key = "mock_access_key"
   secret_key = "mock_secret_key"
   region     = "us-east-1"
-  endpoint   = "http://localhost:4566"
-  s3_force_path_style = true
+
+  s3 {
+    endpoint = "http://localhost:4566"
+  }
 }
